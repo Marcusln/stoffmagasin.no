eccf45f3f1b67bc54e83e925b37f59a76a997c48
Marcus Lauritsen <lauritsenmarcus@gmail.com>
2018-04-05 20:50:50 +0200

    date formatting

--- template-parts/content-standard.php
+++ template-parts/content-standard.php
@@ -20,7 +20,7 @@ $month = $months[get_the_date('n')];
 <article id="content-standard" class="frontpage-margin feed-item component portrait feed-box-border hidden-sm-down" style="margin-top: 30px; margin-bottom: 30px;">
 <a class="no-decoration frontpage-margin feed-item component portrait feed-box-border" href="<?php echo $postUrl; ?>" style="">
    <div class="feed-image-container" style="">
-      <img style="" class="feed-image inline img-fluid" src="<?php the_post_thumbnail_url( array(600,9999) ); ?>">
+      <img style="" class="feed-image inline img-fluid" src="<?php the_post_thumbnail_url( array(900,9999) ); ?>">
   </div>
   <div class="feed-text-container" style="" >
     <span class="graytext kategori">
@@ -42,7 +42,7 @@ $month = $months[get_the_date('n')];
 <article id="content-standard" class="hidden-md-up feed-item component portrait" style="">
 <a class="no-decoration frontpage-margin feed-item component portrait feed-box-border" href="<?php echo $postUrl; ?>" style="">
    <div class="feed-image-container" style="">
-      <img style="" class="feed-image inline img-fluid" src="<?php the_post_thumbnail_url( array(600,9999) ); ?>">
+      <img style="" class="feed-image inline img-fluid" src="<?php the_post_thumbnail_url( array(900,9999) ); ?>">
   </div>
   <div class="feed-text-container" style="" >
     <span class="graytext" style="font-size: 12px; font-weight: 100;
@@ -52,9 +52,11 @@ $month = $months[get_the_date('n')];
         <span class="" style="font-size: 26px; font-family: 'Ludacrys', serif"><?php echo $postTitle; ?></span><br style="line-height: 2vh;">
         <span style="font-size: 16px; line-height: 1.25;" class=""><?php echo $trimmedExcerpt; ?></span>
         <?php if ( get_field( 'journalist' ) ) {
-          echo "<br /><span style='font-style: italic; color: rgb(128, 128, 128); font-weight: 100; font-size: 10px;'>&mdash;" . ' ' . get_field( 'journalist' ) . "</span>";
-        }          echo "<span style='font-style: italic; color: rgb(128, 128, 128); font-weight: 100; font-size: 10px;'><br />" . $dateDay . ". " . $month . " " . $dateYear . "</span>";
-?>
+          echo "<br /><span style='font-style: italic; color: rgb(128, 128, 128); font-weight: 100;'>&mdash; " . get_field( 'journalist' ) . ", " . $dateDay . ". " . $month . " " . $dateYear . "</span>";
+        } else {
+          echo "<span style='font-style: italic; color: rgb(128, 128, 128); font-weight: 100;'><br />" . $dateDay . ". " . $month . " " . $dateYear . "</span>";
+        }
+         ?>
   </div>
 </a>
 </article>
