22e3e0adabf1cf3869d9368a2848f61d0c8feb26
Marcus Lauritsen <lauritsenmarcus@gmail.com>
2018-04-05 20:59:01 +0200

    remove byline mobile

--- template-parts/content-standard.php
+++ template-parts/content-standard.php
@@ -51,12 +51,7 @@ $month = $months[get_the_date('n')];
     </span>
         <span class="" style="font-size: 26px; font-family: 'Ludacrys', serif"><?php echo $postTitle; ?></span><br style="line-height: 2vh;">
         <span style="font-size: 16px; line-height: 1.25;" class=""><?php echo $trimmedExcerpt; ?></span>
-        <?php if ( get_field( 'journalist' ) ) {
-          echo "<br /><span style='font-style: italic; color: rgb(128, 128, 128); font-weight: 100; font-size: 10px;'>&mdash; " . get_field( 'journalist' ) . ", " . $dateDay . ". " . $month . " " . $dateYear . "</span>";
-        } else {
-          echo "<span style='font-style: italic; color: rgb(128, 128, 128); font-weight: 100; font-size: 10px;'><br />" . $dateDay . ". " . $month . " " . $dateYear . "</span>";
-        }
-         ?>
+        <?php echo "<span style='font-style: italic; color: rgb(128, 128, 128); font-weight: 100; font-size: 10px;'><br />" . $dateDay . ". " . $month . " " . $dateYear . "</span>"; ?>
   </div>
 </a>
 </article>
