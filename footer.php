d6c37c6fbaa9517815887fc8ab975bb77d968d63
Marcus Lauritsen <lauritsenmarcus@gmail.com>
2018-04-09 22:28:32 +0200

    temp fix for footer mobile

--- footer.php
+++ footer.php
@@ -15,8 +15,8 @@
 	</div><!-- #content -->
 </div> <!-- #hide-bellow-om-oss -->
 
-<div class="footer" style="">
-	<div class="">
+<div class="footer" style="height: auto; padding: 30px;">
+	<div class="footer-text">
 <br />
 	<div class="" style="font-size: 30px; color: white; display: flex; flex-direction: row; flex-wrap: nowrap; justify-content: center; ">
         <a href="https://www.facebook.com/STOFFmagasin/" target="_blank">
@@ -30,23 +30,25 @@
         </a> 
   	</div><br /><br />
 
-    <div style="display: flex; flex-wrap: wrap; margin: 15px; justify-content: center;">
-      <div style="width: calc(420px + 20px);">
-        Stoff arbeider etter Vær Varsom-plakatens regler for god presseskikk, og får støtte fra Velferdstinget i Bergen.<br />
-      </div>
-      <div style="width: 420px;">
-        Ansvarlig redaktør: Ingrid Marie Vikhammer Sandvik.<br />Samfunnsredaktør: Anne Aase Rokkan <br />Illustrasjonsansvarlig: Ida Neverdahl <br />Fotoredaktør: Henrik Follesø Egeland
-      </div>
-    </div>
-
-    <div style="width: 100vw; text-align: center;">
-      <span>
-        Nettredaktør: Joakim Knudsen. Utviklet av Marcus Lauritsen<br />
-        Om oss | Kontakt </span>
-    </div>
-
+		<span>Stoff arbeider etter Vær Varsom-plakatens regler for god presseskikk, og får støtte fra Velferdstinget i Bergen.<br /><br />Ansvarlig redaktør: Ingrid Marie Vikhammer Sandvik. Nettredaktør: Joakim Knudsen<br />Utviklet av Marcus Lauritsen<br />&copy; Stoff 2018</span>
 	</div>
 </div>
+<!--
+
+
+	<div id="footer" style="display: none; border-bottom: 10px solid black; width: 100vw; height: 50vw; margin-top: 100px; background: url('/wp-content/uploads/2017/04/stoff.png') repeat-x;">
+</div>
+
+<script
+  src="https://code.jquery.com/jquery-3.2.1.min.js"
+  integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
+  crossorigin="anonymous"></script>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+ <script src="http://stof.bergenchallenge.no/wp-content/themes/stoffu/js/functions.js" type="text/javascript"></script>
+-->
+
+
 
 </body>
 </html>
