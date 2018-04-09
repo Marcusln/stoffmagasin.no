4d754b91110fef0fa4f039b2c98308eddb0c6494
Marcus Lauritsen <lauritsenmarcus@gmail.com>
2018-04-09 05:58:25 +0200

    draft footer

--- footer.php
+++ footer.php
@@ -15,8 +15,8 @@
 	</div><!-- #content -->
 </div> <!-- #hide-bellow-om-oss -->
 
-<div class="footer" style="height: auto; padding: 30px;">
-	<div class="footer-text">
+<div class="footer" style="">
+	<div class="">
 <br />
 	<div class="" style="font-size: 30px; color: white; display: flex; flex-direction: row; flex-wrap: nowrap; justify-content: center; ">
         <a href="https://www.facebook.com/STOFFmagasin/" target="_blank">
@@ -30,7 +30,21 @@
         </a> 
   	</div><br /><br />
 
-		<span>Stoff arbeider etter Vær Varsom-plakatens regler for god presseskikk, og får støtte fra Velferdstinget i Bergen.<br />Ansvarlig redaktør: Ingrid Marie Vikhammer Sandvik. Nettredaktør: Joakim Knudsen<br />Utviklet av Marcus Lauritsen<br />&copy; Stoff 2018</span>
+    <div style="display: flex; flex-wrap: wrap; margin: 15px; justify-content: center;">
+      <div style="width: calc(420px + 20px);">
+        Stoff arbeider etter Vær Varsom-plakatens regler for god presseskikk, og får støtte fra Velferdstinget i Bergen.<br />
+      </div>
+      <div style="width: 420px;">
+        Ansvarlig redaktør: Ingrid Marie Vikhammer Sandvik.<br />Samfunnsredaktør: Anne Aase Rokkan <br />Illustrasjonsansvarlig: Ida Neverdahl <br />Fotoredaktør: Henrik Follesø Egeland
+      </div>
+    </div>
+
+    <div style="width: 100vw; text-align: center;">
+      <span>
+        Nettredaktør: Joakim Knudsen. Utviklet av Marcus Lauritsen<br />
+        Om oss | Kontakt </span>
+    </div>
+
 	</div>
 </div>
 
