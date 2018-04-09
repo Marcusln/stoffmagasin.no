22a1658d198aa58d00c099944619075ead3bd9e5
Marcus Lauritsen <lauritsenmarcus@gmail.com>
2018-04-09 22:30:05 +0200

    om oss text

--- pages-templates/om-oss.php
+++ pages-templates/om-oss.php
@@ -11,37 +11,18 @@ get_header();
 
   <div class="" style="display: flex; flex-wrap: nowrap;">
     <div class="hidden-md-down col-6 "  style="display: flex; flex-direction: column; text-align: justify; font-family: 'Arapey', serif; font-size: 18px; line-height: 30px; font-weight: 400;">
-      <p style="">Stoff magasin ble startet tidlig i 2015 av en gjeng NHH-studenter som ønsket å lage en unik, uavhengig og uredd studentavis. Første utgave ble lansert 20.03.15. Avisen satser på dyptgående, modig og kreativ journalistikk innenfor kultur, økonomi og samfunn. Stoff består av studenter som jobber frivillig og ulønnet som journalister, fotografer og grafikere. Redaksjonen har erfaring fra andre store aviser som blant annet Dagens Næringsliv, Bergens Tidende, Aftenposten og Finansavisen.</p>
+      <p style="">Stoff <a class="underline" href="https://www.dn.no/etterBors/2015/03/10/2157/Medier/studentavisopprr-i-bergen" target="_blank">ble</a> <a class="underline" href="http://www.nattogdag.no/2015/03/stoff-magasin-pungshots-dekning-og-kvalitetsjournalistikk/" target="_blank">startet</a> <a class="underline" href="http://www.bt.no/folk/Lager-avis-fra-hybelen-3328137.html?xtor=RSS-2" target="_blank">tidlig</a> i 2015 av en gjeng NHH-studenter som ønsket å lage en unik, uavhengig og uredd studentavis. Første utgave ble lansert 20.03.15. Avisen satser på dyptgående, modig og kreativ journalistikk innenfor kultur, økonomi og samfunn. Stoff består av studenter fra hele student-Bergen som jobber frivillig og ulønnet som journalister, fotografer og grafikere. Redaksjonen har erfaring fra andre store aviser som blant annet Dagens Næringsliv, Bergens Tidende, Aftenposten og Finansavisen.</p>
 
       <p style="">
         Stoff blir utgitt i fire papirutgaver i hvert akademiske semester, og blir distribuert i Bergen.
       </p>
 
-      <p style="display: flex; font-weight: 800;">
-        
-      </p>
-
-      <p style="">
-        <span style="font-weight: bold;">Medieoppslag</span><br/>
-        Dagens Næringsliv &mdash; <a class="underline" href="https://www.dn.no/etterBors/2015/03/10/2157/Medier/studentavisopprr-i-bergen" target="_blank">Studentavisopprør i Bergen</a><br />
-
-        Bergensavisen &mdash; <a class="underline" href="http://www.ba.no/Mener_NHHs_studentavis__Bulle__er_et_smiskeorgan-5-8-35800.html" target="_blank">Mener NHHs studentavis «Bulle» er et smiskeorgan</a><br />
-
-        Natt & Dag &mdash; <a class="underline" href="http://www.nattogdag.no/2015/03/stoff-magasin-pungshots-dekning-og-kvalitetsjournalistikk/" target="_blank">Pungshots-dekning og kvalitetsjournalistikk </a><br />
-
-        Inside &mdash; <a class="underline" href="http://www.inside24.no/k7-bulletin-og-studvest-har-fatt-ny-konkurrent/" target="_blank">K7 Bulletin og Studvest har fått ny konkurrent</a><br />
-
-        Studvest &mdash; <a class="underline" href="http://www.studvest.no/nytt-stoff-pa-markedet/" target="_blank">Nytt Stoff på markedet</a><br />
-
-        Studentradioen i Bergen &mdash; <a class="underline" href="http://srib.no/2015/03/24/nytt-stoff-pa-markedet/" target="_blank">Nytt Stoff på markedet</a><br />
-
-        Bergens Tidende &mdash; <a class="underline" href="http://www.bt.no/folk/Lager-avis-fra-hybelen-3328137.html?xtor=RSS-2" target="_blank">Lager avis fra hybelen</a><br />
-      </p>
-
       <p style="font-weight: 400;">Kontakt oss på <a class="underline" href="mailto:red@stoffmagasin.no">red@stoffmagasin.no</a>.</p>
+
+      <div style="font-family: 'Ludacrys', serif; font-size: 26px; padding: 15px; text-align: center; color: white !important; background-color: black !important; ">Ønsker du å annonsere i Stoff? Ta kontakt på <a href="mailto:red@stoffmagasin.no" style="color: white !important; "> red@stoffmagasin.no</a></div>
     </div>
     <div class="hidden-md-down col-6" style="font-family: 'Arapey', serif; font-size: 18px; line-height: 30px; font-weight: 400;">
-
+<p>
 <b>Ansvarlig redaktør</b> Ingrid Marie Vikhammer Sandvik <br />
 
 <b>Daglig leder </b> <a class="underline" href="mailto:hsharam@gmail.com">Hans Sebastian Haram</a><br />
@@ -64,9 +45,10 @@ get_header();
 
 <b>Fotografer</b> Fredrik Geving Bedsvaag, Henrik Follesø Egeland, Petter Lysgaard<br />
 
-<b>Redaksjon</b> Mathias Juell Johnsen, Olve Hagen Wold, Tellef Solbakk Raabe, Audun Brendbekken, Lisa Aase Rokkan, Mats Vederhus, Ingeborg Katie Åtland, Torbjørn Sandmo, Ola Olsen Lysgaard, Martin Hjelle, Tellef Raabe, Anna Eitrem, Camilla Iversen<br /><br />
+<b>Redaksjon</b> Mathias Juell Johnsen, Olve Hagen Wold, Tellef Solbakk Raabe, Audun Brendbekken, Lisa Aase Rokkan, Mats Vederhus, Ingeborg Katie Åtland, Torbjørn Sandmo, Ola Olsen Lysgaard, Martin Hjelle, Tellef Raabe, Anna Eitrem, Camilla Iversen
+</p>
 
-<div style="font-family: 'Ludacrys', serif; font-size: 26px; padding: 15px; text-align: center; color: white !important; background-color: black !important; ">Ønsker du å annonsere i Stoff? Ta kontakt på <a href="mailto:red@stoffmagasin.no" style="color: white !important; "> red@stoffmagasin.no</a></div>
+
 
     </div>
   </div>
