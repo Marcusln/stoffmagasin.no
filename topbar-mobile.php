62eb8aff544b85428b7a4e27b2df5cae8c57d717
Marcus Lauritsen <lauritsenmarcus@gmail.com>
2018-04-09 06:29:46 +0200

    fix layout for mobile

--- topbar-mobile.php
+++ topbar-mobile.php
@@ -11,21 +11,21 @@
   </div>
 </div>
 
-<div id="fullscreen-menu-mobile" class=" hidden-lg-up initiallyHidden" style="width: 100vw; height: 100vh; background-color: black; z-index: 1030; position: fixed;">
-	<div id="" class="fixed-top hidden-lg-up" style="background-color: black; width: 100%; height: 50px; display: flex; justify-content: space-between; align-items: center;  border-bottom: 1px solid black;">
+<div id="fullscreen-menu-mobile" class=" hidden-lg-up initiallyHidden" style="width: 100vw; height: 120vh; background-color: black; z-index: 1030; position: fixed;">
+	<div id="" class="fixed-top hidden-lg-up" style="background-color: black; width: 100%; height: 50px; display: flex; justify-content: space-between; align-items: center;">
 	  <div class="">
 	    <a href="/index.php">
 	    	<img src="/wp-content/uploads/2017/04/stoff.png" class="" style="align-self: left; height: 30px; padding: 0 3px 0 15px; filter: invert(100%);">
 	    </a>
 	  </div>
 	  <div>
-	  	<a class="" style="">
-	        <i id="menu-mobile-close" class="fa fa-times topbar-icon" style="font-size: 30px; padding: 0 15px 0 3px; color: white;"></i>
+	  	<a id="menu-mobile-close" class="" style="height: 100%; width: 50px;">
+	        <i class="fa fa-times topbar-icon" style="font-size: 30px; padding: 0 15px 0 3px; color: white;"></i>
 	      </a>
 	  </div>
 	</div>
 
-	<div id="mobile-menu-links" style="width: 100%; height: 100%; display: flex; flex-direction: column; align-items: center; justify-content: center;">
+	<div id="mobile-menu-links" style="width: 100%; height: 80vh; display: flex; flex-direction: column; align-items: center; justify-content: center;">
 		<div style="display: flex;">
 			<a href="/samfunn/" style="">
 			Samfunn<br />
@@ -58,7 +58,7 @@
 		</div>
 	</div>
 	
-	<div class="" style="font-size: 40px; color: white; position: absolute; bottom: 15px; right: 15px; display: flex; flex-direction: row; flex-wrap: nowrap;">
+	<div class="" style="font-size: 6.5vh; color: white; position: absolute; bottom: 25vh; right: 5vw; display: flex; flex-direction: row; flex-wrap: nowrap;">
 	      <a href="https://www.facebook.com/STOFFmagasin/" target="_blank">
 	        <i class="fa fa-facebook cursor-pointer-hover" style="padding: 0 10px 15px 10px; color: #3b5998 !important;" aria-hidden="true"></i>
 	      </a>
