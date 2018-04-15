4ca03ff5cb1d07a1d876d1cc9b94b3c52c29dcb8
Marcus Lauritsen <lauritsenmarcus@gmail.com>
2018-04-15 03:28:58 +0200

    cleanup

--- single.php
+++ single.php
@@ -5,26 +5,15 @@
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
  *
  * @package Stoffu
- * This is not in use
+ * 
  */
 
-?>
-
-		
-
-		<?php
 		while ( have_posts() ) : the_post();
 
 			get_template_part( 'template-parts/content', get_post_format() );
-?>
-<!--			the_post_navigation();-->
-<?php
+
 		endwhile; // End of the loop.
-		?>
 
-		</main><!-- #main -->
-
-<?php
 
 get_footer( 'single' );
 
