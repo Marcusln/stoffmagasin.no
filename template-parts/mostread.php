8cc27e741de4113afcc7723e60defef1edfa697d
Marcus Lauritsen <lauritsenmarcus@gmail.com>
2018-04-22 14:25:21 +0200

    fetch scaled images from CDN

--- template-parts/mostread.php
+++ template-parts/mostread.php
@@ -1,35 +1,3 @@
-<!--
-<section class="rad-artikler hidden-md-down">
-  <div class="container-fluid mx-auto" style=""> 
-    <div class="row">
-    <div class="col-xl-3 col-lg-3 border">
-    linjen er kommentert bort            <div class="vertical-text" style="">Mest lest</div>
-    </div>
-      <div class="col-lg-4 col-xl-3 col-12 border sm-text-center padding-sides-1vw" style="padding-bottom: 2vh">
-        <img src="/images/hai1.jpg" class="img-fluid " style="padding-bottom: 2vh;">
-        <br>
-        <span class="graytext kategori " style="">SAMFUNNSSTOFF</span><br>
-        <span class="liten-overskrift " style=""> Haien møter Henning Warloe</span>
-      </div>
-      <div class="col-lg-4 col-xl-3 col-12  sm-text-center padding-sides-1vw" style="padding-bottom: 2vh">
-        <img src="/images/tvskjermen.jpg" class="img-fluid " style="padding-bottom: 2vh;">
-        <br>
-        <span class="graytext kategori " style="">KULTURSTOFF</span><br>
-        <span class="liten-overskrift " style="">Fra TV-skjermen til Kronbar</span>
-      </div>
-      <div class="col-lg-3 col-xl-3 col-12  sm-text-center padding-sides-1vw" style="">
-        <img src="/images/shakespeareogempatien.jpg" class="img-fluid " style="padding-bottom: 2vh;">
-        <br>
-        <span class="graytext kategori " style="">KULTURSTOFF</span><br>
-        <span class="liten-overskrift" style="">Shakespeare og empatien</span>
-      </div>
-      <div class="col-xl-3">
-        tekst
-      </div>
-    </div>
-  </div>
-</section>
--->
 <!-- Overskrift -->
 <div class="frontpage-margin hidden-md-down" style="margin-top: 5vh !important; display: flex; align-items: center;">
   <div id="mest-lest" class="mest-lest">
@@ -40,18 +8,27 @@
 <!-- Container / Flexbox -->
 <article class="most-read-container hidden-md-down" style="display: flex;">
 
+<?php 
+$image1 = get_post_thumbnail_id( 6873 );
+$image1Url = wp_get_attachment_url( $image1 );
+$image2 = get_post_thumbnail_id( 6807 );
+$image2Url = wp_get_attachment_url( $image2 );
+$image3 = get_post_thumbnail_id( 6032 );
+$image3Url = wp_get_attachment_url( $image3 );
+?>
+
 <!-- Box 1/3 -->
 <div class="most-read-article" style="width: 30%;">
-<a class="no-decoration" href="/?p=6032" style="text-decoration: none; color: black;">
+<a class="no-decoration" href="/?p=6873" style="text-decoration: none; color: black;">
   <div class="" style="overflow: hidden;">
-    <img class="" style="height: 300px; min-width: 100%; object-fit: cover;" src="/wp-content/uploads/2018/03/171110_HFE_Kamelen_WEB_04-2.jpg">
+    <img class="" style="height: 300px; min-width: 100%; object-fit: cover;" src="https://i2.wp.com/www.stoffmagasin.no/wp-content/uploads/2018/04/TerjeS%C3%B8viknes_Montasje-1.jpg?fit=600%2C450&ssl=1">
   </div>
     <div class="" style="padding: 5% 5% 0 5%; align-self: baseline;">
       <span class="graytext kategori" style="font-size: 14px !important;">
-        KULTURSTOFF
+        KOMMENTARSTOFF
       </span><br />
       <span class="nestensvart" style="font-size: 25px; font-family: 'Ludacrys'; ">
-        Ka me Kamelen?
+        <?php echo get_the_title( $post = 6873 ); ?>
       </span>
     </div><!--
     <div style="padding: 0 5% 5% 5%; align-self: baseline;">
@@ -64,16 +41,16 @@
 
 <!-- Box 2/3 -->
 <div class="most-read-article" style="width: 30%;">
-<a class="no-decoration" href="/?p=6378" style="text-decoration: none; color: black;">
+<a class="no-decoration" href="/?p=6807" style="text-decoration: none; color: black;">
   <div class="" style="overflow: hidden;">
-    <img class="" style="height: 300px; min-width: 100%; object-fit: cover;" src="/wp-content/uploads/180207_HFE_ARKIVFOTO_TrakkaseringPaaNHH_Nettsak_WEB_-2.jpg">
+    <img class="" style="height: 300px; min-width: 100%; object-fit: cover;" src="https://i0.wp.com/www.stoffmagasin.no/wp-content/uploads/2018/04/stoff_aseksualitet-1.png?fit=600%2C600&ssl=1">
   </div>
     <div class="" style="padding: 5% 5% 0 5%; align-self: baseline;">
       <span class="graytext kategori" style="font-size: 14px !important;">
-        KOMMENTARSTOFF
+        SAMFUNNSTOFF
       </span><br />
       <span class="nestensvart" style="font-size: 25px; font-family: 'Ludacrys'; ">
-        NHH sover i sannhetens time
+        <?php echo get_the_title( $post = 6807 ); ?>
       </span>
     </div><!--
     <div style="padding: 0 5% 5% 5%; align-self: baseline;">
@@ -86,16 +63,16 @@
 
 <!-- Box 3/3 -->
 <div class="most-read-article" style="width: 30%;">
-  <a class="no-decoration" href="/?p=6219" style="text-decoration: none; color: black;">
+  <a class="no-decoration" href="/?p=6032" style="text-decoration: none; color: black;">
   <div class="" style="overflow: hidden;">
-    <img class="" style="height: 300px; min-width: 100%; object-fit: cover;" src="/wp-content/uploads/2018/03/fylla-har-aldri-skylda.jpg">
+    <img class="" style="height: 300px; min-width: 100%; object-fit: cover;" src="https://i0.wp.com/www.stoffmagasin.no/wp-content/uploads/2017/11/kamelen.jpg?fit=600%2C2450">
   </div>
     <div class="" style="padding: 5% 5% 0 5%; align-self: baseline;">
       <span class="graytext kategori" style="font-size: 14px !important;">
-        KOMMENTARSTOFF
+        KULTURSTOFF
       </span><br />
       <span class="nestensvart" style="font-size: 25px; font-family: 'Ludacrys'; ">
-        Fylla har aldri skylda
+        <?php echo get_the_title( $post = 6032 ); ?>
       </span>
     </div><!--
     <div style="padding: 0 5% 5% 5%; align-self: baseline;">
@@ -106,26 +83,3 @@
   </a>
 </div>
 </article>
-
-<!--
-<div style="animation: markets-bar-marquee 85.82s linear infinite running;">
-  Masse tekst Ary spiller på kronbar på fredag. I den anledning slo hun av en prat med Stoff om ny musikk.Ary spiller på kronbar på fredag. I den anledning slo hun av en prat med Stoff om ny musikk.Ary spiller på kronbar på fredag. I den anledning slo hun av en prat med Stoff om ny musikk.Ary spiller på kronbar på fredag. I den anledning slo hun av en prat med Stoff om ny musikk.Ary spiller på kronbar på fredag. I den anledning slo hun av en prat med Stoff om ny musikk.Ary spiller på kronbar på fredag. I den anledning slo hun av en prat med Stoff om ny musikk.
-</div>
--->
-
-<!-- 
-
-<article class="most-read-container border" style="margin: 0vw 3vw 0vw 3vw; display: flex; flex-direction: row;">
-
-    <div class="border" style="padding: 0vw 3vw 0vw 3vw; flex: 1 1 0; ">
-      <img class="img-fluid" src="http://stof.bergenchallenge.no/wp-content/uploads/2017/08/hai1.jpg">
-    </div>
-   <div class="border" style="padding: 0vw 3vw 0vw 3vw; flex: 1 1 0; flex-direction: column;">
-     <img class="img-fluid" src="http://stof.bergenchallenge.no/wp-content/uploads/2017/08/shakespeareogempatien.jpg">
-   </div>
-   <div class="border" style="padding: 0vw 3vw 0vw 3vw; flex: 1 1 0; flex-direction: column;">
-     <img class="img-fluid" src="http://stof.bergenchallenge.no/wp-content/uploads/2017/04/tvskjermen.jpg">
-  </div>
-</article>
-
--->
