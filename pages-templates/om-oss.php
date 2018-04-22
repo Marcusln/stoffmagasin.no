df84106b6fc3fc9024b49486751e077fa49ada97
Marcus Lauritsen <lauritsenmarcus@gmail.com>
2018-04-22 14:23:13 +0200

    text

--- pages-templates/om-oss.php
+++ pages-templates/om-oss.php
@@ -11,7 +11,7 @@ get_header();
 
   <div class="" style="display: flex; flex-wrap: nowrap;">
     <div class="hidden-md-down col-6 "  style="display: flex; flex-direction: column; text-align: justify; font-family: 'Arapey', serif; font-size: 18px; line-height: 30px; font-weight: 400;">
-      <p style="">Stoff <a class="underline" href="https://www.dn.no/etterBors/2015/03/10/2157/Medier/studentavisopprr-i-bergen" target="_blank">ble</a> <a class="underline" href="http://www.nattogdag.no/2015/03/stoff-magasin-pungshots-dekning-og-kvalitetsjournalistikk/" target="_blank">startet</a> <a class="underline" href="http://www.bt.no/folk/Lager-avis-fra-hybelen-3328137.html?xtor=RSS-2" target="_blank">tidlig</a> i 2015 av en gjeng NHH-studenter som ønsket å lage en unik, uavhengig og uredd studentavis. Første utgave ble lansert 20.03.15. Avisen satser på dyptgående, modig og kreativ journalistikk innenfor kultur, økonomi og samfunn. Stoff består av studenter fra hele student-Bergen som jobber frivillig og ulønnet som journalister, fotografer og grafikere. Redaksjonen har erfaring fra andre store aviser som blant annet Dagens Næringsliv, Bergens Tidende, Aftenposten og Finansavisen.</p>
+      <p style="">Stoff <a class="underline" href="https://www.dn.no/etterBors/2015/03/10/2157/Medier/studentavisopprr-i-bergen" target="_blank">ble</a> <a class="underline" href="http://www.nattogdag.no/2015/03/stoff-magasin-pungshots-dekning-og-kvalitetsjournalistikk/" target="_blank">startet</a> <a class="underline" href="http://www.bt.no/folk/Lager-avis-fra-hybelen-3328137.html?xtor=RSS-2" target="_blank">tidlig</a> i 2015 av en gjeng studenter som ønsket å lage en unik, uavhengig og uredd studentavis. Første utgave ble lansert 20.03.15. Avisen satser på dyptgående, modig og kreativ journalistikk innenfor kultur, økonomi og samfunn. Stoff består av studenter fra hele student-Bergen som jobber frivillig og ulønnet som journalister, fotografer og grafikere. Redaksjonen har erfaring fra andre store aviser som blant annet Dagens Næringsliv, Bergens Tidende, Aftenposten og Finansavisen.</p>
 
       <p style="">
         Stoff blir utgitt i fire papirutgaver i hvert akademiske semester, og blir distribuert i Bergen.
@@ -61,7 +61,7 @@ get_header();
 
   <div class="" style="display: flex; flex-wrap:  wrap;">
     <div class="hidden-lg-up col-12 "  style="display: flex; flex-direction: column; text-align: left; font-family: 'Arapey', serif; font-size: 18px; line-height: 30px; font-weight: 400;">
-      <p style="">Stoff magasin ble startet tidlig i 2015 av en gjeng NHH-studenter som ønsket å lage en unik, uavhengig og uredd studentavis. Første utgave ble lansert 20.03.15. Avisen satser på dyptgående, modig og kreativ journalistikk innenfor kultur, økonomi og samfunn. Stoff består av studenter som jobber frivillig og ulønnet som journalister, fotografer og grafikere. Redaksjonen har erfaring fra andre store aviser som blant annet Dagens Næringsliv, Bergens Tidende, Aftenposten og Finansavisen.</p>
+      <p style="">Stoff <a class="underline" href="https://www.dn.no/etterBors/2015/03/10/2157/Medier/studentavisopprr-i-bergen" target="_blank">ble</a> <a class="underline" href="http://www.nattogdag.no/2015/03/stoff-magasin-pungshots-dekning-og-kvalitetsjournalistikk/" target="_blank">startet</a> <a class="underline" href="http://www.bt.no/folk/Lager-avis-fra-hybelen-3328137.html?xtor=RSS-2" target="_blank">tidlig</a> i 2015 av en gjeng studenter som ønsket å lage en unik, uavhengig og uredd studentavis. Første utgave ble lansert 20.03.15. Avisen satser på dyptgående, modig og kreativ journalistikk innenfor kultur, økonomi og samfunn. Stoff består av studenter fra hele student-Bergen som jobber frivillig og ulønnet som journalister, fotografer og grafikere. Redaksjonen har erfaring fra andre store aviser som blant annet Dagens Næringsliv, Bergens Tidende, Aftenposten og Finansavisen.</p>
 
       <p style="">
         Stoff blir utgitt i fire papirutgaver i hvert akademiske semester, og blir distribuert i Bergen.
@@ -73,49 +73,31 @@ get_header();
 
       <div class=" " style="font-family: 'Arapey', serif; font-size: 18px; line-height: 30px; font-weight: 400;">
         <p>
-          <b style="font-size: 26px;">Redaksjonen</b> <br />
-        <b>Ansvarlig redaktør</b> Ingrid Marie Vikhammer Sandvik <br />
+          <b>Ansvarlig redaktør</b> Ingrid Marie Vikhammer Sandvik <br />
 
-        <b>Daglig leder </b> <a class="underline" href="mailto:hsharam@gmail.com">Hans Sebastian Haram</a><br />
+          <b>Daglig leder </b> <a class="underline" href="mailto:hsharam@gmail.com">Hans Sebastian Haram</a><br />
 
-        <b>Kulturredaktør</b> Anne Aase Rokkan<br />
+          <b>Samfunnsredaktør</b> Anne Aase Rokkan<br />
 
-        <b>Illustrasjonsansvarlig</b> Ida Neverdahl<br />
+          <b>Illustrasjonsansvarlig</b> Ida Neverdahl<br />
 
-        <b>Styreleder</b> Mathias Juell Johnsen<br />
+          <b>Styreleder</b> Mathias Juell Johnsen<br />
 
-        <b>Utviklingsredaktør</b> Marcus Lauritsen<br />
+          <b>Utviklingsredaktør</b> Marcus Lauritsen<br />
 
-        <b>Nettredaktør</b> Joakim Knudsen<br />
+          <b>Nettredaktør</b> Joakim Knudsen<br />
 
-        <b>Art Director</b> Tove Huldén<br />
+          <b>Art Director</b> Tove Huldén<br />
 
-        <b>Grafikere</b> Ida Woldsund og Frida Annette Helseth Strømme<br />
+          <b>Grafikere</b> Ida Woldsund og Frida Annette Helseth Strømme<br />
 
-        <b>Fotoredaktør</b> Maiken Larsen Solholmvik<br />
+          <b>Fotoredaktør</b> Henrik Follesø Egeland<br />
 
-        <b>Fotografer</b> Fredrik Geving Bedsvaag, Henrik Follesø Egeland, Petter Lysgaard<br />
+          <b>Fotografer</b> Fredrik Geving Bedsvaag, Henrik Follesø Egeland, Petter Lysgaard<br />
 
-        <b>Redaksjon</b> Mathias Juell Johnsen, Olve Hagen Wold, Tellef Solbakk Raabe, Audun Brendbekken, Lisa Aase Rokkan, Mats Vederhus, Ingeborg Katie Åtland, Torbjørn Sandmo, Ola Olsen Lysgaard, Martin Hjelle, Tellef Raabe, Anna Eitrem, Camilla Iversen
-        </p><br /><br />
+          <b>Redaksjon</b> Mathias Juell Johnsen, Olve Hagen Wold, Tellef Solbakk Raabe, Audun Brendbekken, Lisa Aase Rokkan, Mats Vederhus, Ingeborg Katie Åtland, Torbjørn Sandmo, Ola Olsen Lysgaard, Martin Hjelle, Tellef Raabe, Anna Eitrem, Camilla Iversen
+        </p>
       </div>
-
-      <p style="text-align: left;">
-        <b style="font-size: 26px;">Medieoppslag</b><br/>
-        Dagens Næringsliv &mdash;<br/> <a class="underline" href="https://www.dn.no/etterBors/2015/03/10/2157/Medier/studentavisopprr-i-bergen" target="_blank">Studentavisopprør i Bergen</a><br /><br />
-
-        Bergensavisen &mdash;<br/> <a class="underline" href="http://www.ba.no/Mener_NHHs_studentavis__Bulle__er_et_smiskeorgan-5-8-35800.html" target="_blank">Mener NHHs studentavis «Bulle» er et smiskeorgan</a><br /><br />
-
-        Natt & Dag &mdash;<br/> <a class="underline" href="http://www.nattogdag.no/2015/03/stoff-magasin-pungshots-dekning-og-kvalitetsjournalistikk/" target="_blank">Pungshots-dekning og kvalitetsjournalistikk </a><br /><br />
-
-        Inside &mdash;<br/> <a class="underline" href="http://www.inside24.no/k7-bulletin-og-studvest-har-fatt-ny-konkurrent/" target="_blank">K7 Bulletin og Studvest har fått ny konkurrent</a><br /><br />
-
-        Studvest &mdash;<br/> <a class="underline" href="http://www.studvest.no/nytt-stoff-pa-markedet/" target="_blank">Nytt Stoff på markedet</a><br /><br />
-
-        Studentradioen i Bergen &mdash;<br/> <a class="underline" href="http://srib.no/2015/03/24/nytt-stoff-pa-markedet/" target="_blank">Nytt Stoff på markedet</a><br /><br />
-
-        Bergens Tidende &mdash;<br/> <a class="underline" href="http://www.bt.no/folk/Lager-avis-fra-hybelen-3328137.html?xtor=RSS-2" target="_blank">Lager avis fra hybelen</a><br />
-      </p>
 
       <p style="font-weight: 400;">Kontakt oss på <a class="underline" href="mailto:red@stoffmagasin.no">red@stoffmagasin.no</a>.</p>
     </div>
